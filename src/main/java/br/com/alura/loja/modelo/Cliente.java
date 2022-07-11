bc0d01945b9d316e06a16c05d0528d7ebe566565
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-07-11 20:14:12 -0300

    Projeto criado. Aula 1 até a lição 4

---
+++ src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -0,0 +1,44 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String cpf;
+
+	public Cliente(String nome, String cpf) {
+		this.setNome(nome);
+		this.setCpf(cpf);
+	}
+
+	public Cliente() {
+
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
