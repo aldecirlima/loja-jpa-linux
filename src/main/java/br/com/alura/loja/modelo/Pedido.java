fb4c20ca873a4357db211654b8543d45cd02d932
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-07-11 21:02:38 -0300

    Aula 1 concluída

--- src/main/java/br/com/alura/loja/modelo/Pedido.java
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,13 +22,16 @@ public class Pedido {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name="valor_total")
 	private BigDecimal valorTotal;
 	private LocalDate data = LocalDate.now();
 
 	@ManyToOne
 	private Cliente cliente;
 
-	@OneToMany(mappedBy = "pedido")
+//	A anotação cascade ALL indica que todos os items serão salvos em cascata com os items
+//	Sem esta anotação o pedido seria cadastrado solto, sem os items.
+	@OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
 	private List<ItemPedido> itens = new ArrayList<ItemPedido>();
 
 	public Pedido(Cliente cliente) {
