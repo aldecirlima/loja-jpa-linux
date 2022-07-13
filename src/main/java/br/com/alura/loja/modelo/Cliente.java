885df83ae0deabdda288551e3c74fe687b406c8f
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-07-13 19:36:54 -0300

    Aula 05 lição 02 - Classes Embeddable (Embutidas)

--- src/main/java/br/com/alura/loja/modelo/Cliente.java
+++ src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.modelo;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,32 +14,33 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
-	private String cpf;
+
+//	Classe embutida com a anotação @Embedded
+	@Embedded
+	private DadosPessoais dadosPessoais;
 
 	public Cliente(String nome, String cpf) {
-		this.setNome(nome);
-		this.setCpf(cpf);
+		this.dadosPessoais = new DadosPessoais(nome, cpf);
 	}
 
 	public Cliente() {
 
 	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
+	
 	public String getNome() {
-		return nome;
+		return this.dadosPessoais.getNome();
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
 	}
 
 }
---
+++ src/main/java/br/com/alura/loja/modelo/DadosPessoais.java
@@ -0,0 +1,37 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.Embeddable;
+
+// A Classe DadosPessoais é embutível dentro de outra classe (no caso Cliente), 
+// basta anotar a classe embitível com @Embeddable
+@Embeddable
+public class DadosPessoais {
+
+	private String nome;
+	private String cpf;
+
+	public DadosPessoais() {
+	}
+
+	public DadosPessoais(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
